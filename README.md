b0ea9653f57fa72200f8662c47897e0abf651bec
Brandon Malone <bmmalone@gmail.com>
2022-02-06 17:29:39 +0100

    ADD initial project structure

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# peptide-encoder
+# Peptide Encoder
+
 An encoder for peptides (short amino acid sequences) based on BLOSUM similarity.
+
+In particular, this package includes a model for learning peptide embeddings
+such that the embedding of two peptides in the vector space is proportional to
+their BLOSUM62 similarity.
+
+### Installation
+
+This project is written in `python3` and can be installed with `pip`.
+
+```
+pip3 install .
+```
---
+++ tests/test_peptide_encoder.py
@@ -0,0 +1,71 @@
+""" A high-level set of tests for checking the peptide encoder package
+"""
+
+import pytest
+
+import argparse
+import logging
+logger = logging.getLogger(__name__)
+
+###
+# If using the pyllars package, these lines can be used so that the logger
+# actually outputs logging statements across all modules.
+###
+#import pyllars.logging_utils as logging_utils
+#logging_utils.set_logging_values(logging_level='DEBUG')
+
+import pepenc
+
+from typing import Mapping
+
+###
+# Fixtures and other "Arrange" phase helpers
+###
+def get_version() -> str:
+    return pepenc.__version__
+
+@pytest.fixture
+def version() -> str:
+    return get_version()
+
+
+###
+# The actual tests
+###
+
+def test_version(version:str) -> None:
+    """ Ensure we have the correct version
+
+    N.B. Since we have a fixture called `version`, that will be passed as
+    the "`version`" parameter when running through pytest.
+
+    Parameters
+    ----------
+    version : str
+        A string representation of the version of `aa_encode`
+
+    Returns
+    -------
+    None : None
+        We assert that the version matches our expected version
+    """
+
+    # we do not really have an "Act" phase in this simple test
+
+    # "Assert" that we have the expected behavior
+    expected_version = '0.1.0'
+    assert (expected_version == version)
+    
+def run_all():
+    """ Run all of the tests
+
+    This function is useful in case we want to run our tests outside of the
+    pytest framework.
+    """
+    # since we are not running through pytest, we have to grab the inputs to the
+    # tests
+    version = get_version()
+    test_version(version)
+
+if __name__ == '__main__':
+    run_all()
